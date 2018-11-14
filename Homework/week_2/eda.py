4bd4d7a81e31b9f1231ccfc0f41782ff6c6ac795
[timok1] <[timokoster1@gmail.com]>
2018-11-14 16:35:47 +0100

    week 2

--- Homework/week_2/eda.py
+++ Homework/week_2/eda.py
@@ -4,15 +4,12 @@ import matplotlib.pyplot as plt
 import json
 
 
-#country = 0, region = 1, popdens = 3, infmor = 7, gdp = 8
-
 def check_csv(input):
     with open(input, newline='\n') as in_csv:
         with open('output.csv', 'w') as out_csv:
             reader = csv.DictReader(in_csv)
-
-
-            writer = csv.DictWriter(out_csv, fieldnames=['Country', 'Region', pop_dens,inf_mor,gdp])
+            writer = csv.DictWriter(out_csv, fieldnames=(['Country', 'Region',
+                                    pop_dens, inf_mor, gdp]))
             writer.writeheader()
 
             for line in reader:
@@ -24,75 +21,100 @@ def check_csv(input):
                 if type(line['Region']) != str or not any(c.isalpha() for c in line['Region']):
                     line['Region'] = 'Unknown Region'
 
-                #  Convert pop. dens. to float, if impossible set to 0
-                line[pop_dens] = line[pop_dens].replace(',','.')
+                # Replace comma's in pop. dens. with periods, convert to float,
+                # if impossible set to 0
+                line[pop_dens] = line[pop_dens].replace(',', '.')
                 try:
                     line[pop_dens] = float(line[pop_dens])
-                except:
+                except ValueError:
                     line[pop_dens] = 0
 
-                #  Replace comma's in Infant mortality with periods, convert
+                # Replace comma's in Infant mortality with periods, convert
                 # to float and check
-                line[inf_mor] = line[inf_mor].replace(',','.')
+                line[inf_mor] = line[inf_mor].replace(',', '.')
                 try:
                     line[inf_mor] = float(line[inf_mor])
-                except:
+                except ValueError:
                     line[inf_mor] = 0
 
                 #  Convert GDP to int if possible, else set to 0
-                line[gdp] = line[gdp].replace(' dollars','')
+                line[gdp] = line[gdp].replace(' dollars', '')
                 try:
                     line[gdp] = int(line[gdp])
-                except:
+                except ValueError:
                     line[gdp] = 0
 
-                writer.writerow({'Country': line['Country'], 'Region': line['Region'], pop_dens: line[pop_dens], inf_mor: line[inf_mor], gdp: line[gdp]})
+                # Write info to new csv file line by line
+                (writer.writerow({'Country': line['Country'],
+                            'Region': line['Region'], pop_dens: line[pop_dens],
+                                inf_mor: line[inf_mor], gdp: line[gdp]}))
             in_csv.close()
             out_csv.close()
 
-def gdp_calculation(data_dict):
-    with open(data_dict, newline='\n') as data:
 
-        df = pd.DataFrame(pd.read_csv(data))
-        df_gdp = df.loc[df[gdp] != 0]
-        gdp_sum = df_gdp[gdp].sum()
-        avg_gdp = gdp_sum / (len(df_gdp))
-        gdp_median = df_gdp[gdp].median()
-        gdp_mode = df_gdp[gdp].mode()
-        gdp_std = df_gdp[gdp].std()
+def gdp_calculation():
+    # Create new dataframe for just gdp, ignore 0 values
+    df_gdp = df[gdp].loc[df[gdp] != 0]
 
-        plt.hist(df_gdp[gdp], bins=100)
-        plt.xlabel('GDP per capita')
-        plt.ylabel('Number of countries')
-        plt.show()
-    data.close()
+    # Calculate average, median, mode and standard deviation
+    gdp_sum = df_gdp.sum()
+    avg_gdp = gdp_sum / (len(df_gdp))
+    gdp_median = df_gdp.median()
+    gdp_mode = df_gdp.mode()
+    gdp_std = df_gdp.std()
 
-def infmor_calculation(data_dict):
-    with open(data_dict, newline='\n') as data:
-        df = pd.DataFrame(pd.read_csv(data))
-        df_infmor = df.loc[df[inf_mor] != 0]
-        infmor_min = df_infmor[inf_mor].min()
-        infmor_max = df_infmor[inf_mor].max()
-        infmor_quantile = df_infmor[inf_mor].quantile([0.25,0.5,0.75])
-        boxplot = df_infmor.boxplot(column=[inf_mor])
-        plt.show()
+    # Create histogram
+    plt.hist(df_gdp, bins=100)
+    plt.xlabel('GDP per capita')
+    plt.ylabel('Number of countries')
+    plt.show()
+
+    return avg_gdp, gdp_median, gdp_mode, gdp_std
+
+
+def infmor_calculation():
+    # Create new dataframe for just inf_mor, ignore 0 values
+    df_infmor = df[inf_mor].loc[df[inf_mor] != 0]
+
+    # Calculate five number summary
+    infmor_min = df_infmor.min()
+    infmor_max = df_infmor.max()
+    infmor_quantiles = df_infmor.quantile([0.25, 0.5, 0.75])
+    infmor_first = infmor_quantiles[0.25]
+    infmor_median = infmor_quantiles[0.5]
+    infmor_third = infmor_quantiles[0.75]
+    L_fns = [infmor_min, infmor_first, infmor_median, infmor_third, infmor_max]
+
+    # Create boxplot of inf_mor
+    df.loc[df[inf_mor] != 0].boxplot(column=[inf_mor])
+    plt.show()
+
+    return L_fns
+
 
 def write_json(data_dict, output):
     with open(data_dict, newline='\n') as data:
         reader = csv.DictReader(data)
-        file = open(output,'w', encoding='utf-8')
+        file = open(output, 'w')
         for line in reader:
             json.dump(line, file)
             file.write('\n')
-        file.close()
+    file.close()
 
 
 if __name__ == "__main__":
+
     pop_dens = 'Pop. Density (per sq. mi.)'
     inf_mor = 'Infant mortality (per 1000 births)'
     gdp = 'GDP ($ per capita) dollars'
 
     check_csv('input.csv')
-    gdp_calculation('output.csv')
-    infmor_calculation('output.csv')
+
+    # Create dataframe of data
+    with open('output.csv', newline='\n') as data:
+        df = pd.DataFrame(pd.read_csv(data))
+    data.close()
+
+    gdp_calculation()
+    infmor_calculation()
     write_json('output.csv', 'data_json.json')
