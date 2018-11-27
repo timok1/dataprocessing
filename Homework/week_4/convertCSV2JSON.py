ef404632d4343bdf42669a3c6c11af734555fc75
[timok1] <[timokoster1@gmail.com]>
2018-11-27 16:55:56 +0100

    Bijna klaar week 4

---
+++ Homework/week_4/convertCSV2JSON.py
@@ -0,0 +1,33 @@
+import csv
+import json
+
+
+def write_json(input, output, id, league):
+    """
+    Writes data from a csv file (input) to a json (output). 'id' is the key
+    value that is used.
+    """
+
+    with open(input, newline='') as data:
+        reader = csv.DictReader(data)
+        file_w = open(output, 'w')
+        json_list = {}
+        headers = reader.fieldnames
+
+        # For every line create a key and its attributes.
+        for line in reader:
+            if line['League'] == league:
+                child = {}
+                for item in headers:
+                    # Skip id
+                    if item != id:
+                        child[item] = line[item].replace(',', '')
+                json_list[line[id]] = child
+
+        json.dump(json_list, file_w, indent=2)
+    data.close()
+    file_w.close()
+
+
+if __name__ == "__main__":
+    write_json('Home attendance 2017-18.csv', 'eredivisie_attendance.json', 'Team', 'Eredivisie')
